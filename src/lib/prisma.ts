36edfb1485b4ed522ed290b22506bd66d34e3517
Paulo Viana <paulovianalab@gmail.com>
2026-03-29 00:45:03 -0300

    Fix: sincronizando caminho do Prisma gerado

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '../generated';
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 
