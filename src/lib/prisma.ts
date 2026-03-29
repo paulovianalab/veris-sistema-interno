442fd592a8c0a57a52b6e8fce4be398ee47e97d2
Paulo Viana <paulovianalab@gmail.com>
2026-03-29 15:37:52 -0300

    Instalando motor PostgreSQL via integração Neon

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client/web";
 
 const prismaClientSingleton = () => {
-  // Configuração Oficial Turso + Vercel
-  if (process.env.TURSO_DATABASE_URL && process.env.TURSO_DATABASE_URL !== 'undefined') {
-    const libsql = createClient({
-      url: process.env.TURSO_DATABASE_URL,
-      authToken: process.env.TURSO_AUTH_TOKEN,
-    });
-    const adapter = new PrismaLibSQL(libsql as any);
-    return new PrismaClient({ adapter } as any);
-  }
-
-  // Fallback Vercel sem Variaveis definidas
-  if (process.env.NODE_ENV === 'production') {
-     return new PrismaClient();
-  }
-
-  // Development only: Use Prisma SQLite Native
   return new PrismaClient();
 };
 
