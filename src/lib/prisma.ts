7a7151b27d6c1085745afd413375209d24805c1b
Paulo Viana <paulovianalab@gmail.com>
2026-03-29 09:07:04 -0300

    Corrigindo L maiusculo do Prisma 5

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client/web";
 
 const prismaClientSingleton = () => {
@@ -9,7 +9,7 @@ const prismaClientSingleton = () => {
       url: process.env.TURSO_DATABASE_URL,
       authToken: process.env.TURSO_AUTH_TOKEN,
     });
-    const adapter = new PrismaLibSql(libsql as any);
+    const adapter = new PrismaLibSQL(libsql as any);
     return new PrismaClient({ adapter } as any);
   }
 
