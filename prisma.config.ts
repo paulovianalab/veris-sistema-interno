727111ff46496a07f6d5272d5298b9c3a58177dc
Paulo Viana <paulovianalab@gmail.com>
2026-03-29 08:27:36 -0300

    Corrigindo Syntax Parse do Prisma 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
-export default {
-  schema: "prisma/schema.prisma",
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
   earlyAccess: true,
+  schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL || "file:./dummy.db",
-  },
-};
+    url: "file:./dummy.db"
+  }
+});
